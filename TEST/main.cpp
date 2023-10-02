5cffd7b75287daf803ab9f5c89c43961ba80c17c
nagy <nagyy8751@gmail.com>
2023-10-02 10:39:36 +0200

    2-10-2023

--- TEST/main.cpp
+++ TEST/main.cpp
@@ -1,55 +1,30 @@
 #include <iostream>
-using namespace std;
-
-void swap(int *a, int *b);
-void printArr(int arr[], int length);
-int binarySearch(int arr[], int target, int start, int end);
+#include <memory>
+class MyClass
+{
+public:
+    int i;
+    MyClass()
+    { // Constructor
+        std::cout << "\n created\n";
+    }
+    ~MyClass()
+    { // Destructor
+        std::cout << "\n destroyed\n";
+    }
+};
 int main()
 {
-    int nums[] = {1, 2, 3, 4, 5, 6, 7, 8};
-    int len = sizeof(nums) / sizeof(nums[0]);
+    if (true)
+    {
+        // Scope of the following MyClass object is this if-block
+        std::unique_ptr<MyClass> ptr(new MyClass());
 
-    int result = binarySearch(nums, 1000, 0, len);
-    if (result == -1)
-        cout << "not foutn" << endl;
-    cout << "result: " << result << endl;
-
+        ptr->i = 5;
+        std::cout << "\n"
+                  << ptr->i << "\n";
+    }
+    // The pointer gets deallocated automatically at this point.
+    // Thus, the destructor of MyClass is called here.
     return 0;
-}
-
-int binarySearch(int arr[], int target, int start, int end)
-{
-    int middle = start + (end - start) / 2;
-    if (arr[middle] == target)
-    {
-        return middle;
-    }
-    else if (arr[middle] < target)
-    {
-        return binarySearch(arr, target, middle + 1, end);
-    }
-    else if (arr[middle] > target)
-    {
-        return binarySearch(arr, target, start, middle - 1);
-    }
-    else
-    {
-        return -1;
-    }
-}
-void swap(int *a, int *b)
-{
-    int temp = *a;
-    *a = *b;
-    *b = temp;
-};
-
-void printArr(int arr[], int length)
-{
-    std::cout << "Array is : [";
-    for (int i = 0; i < length; i++)
-    {
-        std::cout << arr[i] << '\t';
-    }
-    std::cout << ']';
 }
